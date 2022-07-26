4bf3d92bb660a132f5d556d8b61b6180faf1cee3
Jean <you@example.com>
2022-07-26 14:47:55 -0400

    Adding some changes in the projects dict

--- static/data.py
+++ static/data.py
@@ -7,5 +7,23 @@ projects = {
         "author": "Tim Berners-Lee, inventor of the World Wide Web.",
         "image_src": ["../static/assets/images/work001-01.jpg","../static/assets/images/news_1.png","../static/assets/images/news_2.png"],
         "repo": "https://github.com/jeancarlosLXIX/Scrape_the_source"
+    },
+    "Organizer":{
+        "title": "Organizer",
+        "project_title": "002 : Organizer",
+        "description": "Take a folder with differents files and create a folder for each group (extension based).",
+        "quote": "For every minute spent organizing, an hour is earned.",
+        "author": "Anonymous.",
+        "image_src": ["../static/assets/images/work001-01.jpg","../static/assets/images/news_1.png","../static/assets/images/news_2.png"],
+        "repo": "https://github.com/jeancarlosLXIX/boring-stuff.py/tree/main/PROJECTS/Organizer"
+    },
+    "Test":{
+        "title": "Title",
+        "project_title": "00# : Name",
+        "description": ".",
+        "quote": "Lorem.",
+        "author": "PC.",
+        "image_src": ["../static/assets/images/work001-01.jpg","../static/assets/images/news_1.png","../static/assets/images/news_2.png"],
+        "repo": "https://github.com/jeancarlosLXIX"
     }
 }
